a2efc1dd0cacd01e3906cb7061e3a368f3780fc8
Sanjog Wanem <37484722+Swanem24@users.noreply.github.com>
2020-06-08 12:16:23 +0100

    Add files via upload

---
+++ Pizza Ordering System/PizzaOrderingSystem/PizzaOrderingSystemBlueJ/__SHELL1.java
@@ -0,0 +1,13 @@
+
+public class __SHELL1 extends bluej.runtime.Shell {
+public static java.lang.Object run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("N:\\Desktop\\PizzaOrderingSystem\\PizzaOrderingSystemBlueJ");
+final KeyboardInput keyboard1 = (KeyboardInput)__bluej_runtime_scope.get("keyboard1");
+
+
+try {
+return makeObj(keyboard1.getInputString()
+);}
+finally {
+}
+}}
